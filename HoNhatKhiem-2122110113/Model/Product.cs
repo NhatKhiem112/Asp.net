9cacf0a63b92efc6611bcffe7fb4191d2763c6da
LENOVOLEGION5 <honhutkhiem123@gmail.com>
2025-03-29 08:31:27 +0700

    first commit

---
+++ HoNhatKhiem-2122110113/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace HoNhatKhiem_2122110113.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double price { get; set; }
+        public string Image { get; set; }
+
+    }
+}
