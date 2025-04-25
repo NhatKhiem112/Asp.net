eeea69d3bb3816694fdf59bed09c5cc709c75c01
LENOVO <honhutkhiem123@gmail.com>
2025-04-25 13:32:44 +0700

    updated

--- backend/CineTicket.API/obj/Debug/net8.0/CineTicket.API.AssemblyInfo.cs
+++ backend/CineTicket.API/obj/Debug/net8.0/CineTicket.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineTicket.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f02152d36d921e7d1a65970d7f04ca824c9d2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineTicket.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineTicket.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ cineticket.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th4 25, 2025 lúc 08:31 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `cineticket`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `bookings`
+--
+-- Error reading structure for table cineticket.bookings: #1932 - Table &#039;cineticket.bookings&#039; doesn&#039;t exist in engine
+-- Error reading data for table cineticket.bookings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `cineticket`.`bookings`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `bookingseats`
+--
+-- Error reading structure for table cineticket.bookingseats: #1932 - Table &#039;cineticket.bookingseats&#039; doesn&#039;t exist in engine
+-- Error reading data for table cineticket.bookingseats: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `cineticket`.`bookingseats`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `cinemahalls`
+--
+-- Error reading structure for table cineticket.cinemahalls: #1932 - Table &#039;cineticket.cinemahalls&#039; doesn&#039;t exist in engine
+-- Error reading data for table cineticket.cinemahalls: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `cineticket`.`cinemahalls`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `cinemas`
+--
+-- Error reading structure for table cineticket.cinemas: #1932 - Table &#039;cineticket.cinemas&#039; doesn&#039;t exist in engine
+-- Error reading data for table cineticket.cinemas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `cineticket`.`cinemas`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `movies`
+--
+-- Error reading structure for table cineticket.movies: #1932 - Table &#039;cineticket.movies&#039; doesn&#039;t exist in engine
+-- Error reading data for table cineticket.movies: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `cineticket`.`movies`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `screenings`
+--
+-- Error reading structure for table cineticket.screenings: #1932 - Table &#039;cineticket.screenings&#039; doesn&#039;t exist in engine
+-- Error reading data for table cineticket.screenings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `cineticket`.`screenings`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `seats`
+--
+-- Error reading structure for table cineticket.seats: #1932 - Table &#039;cineticket.seats&#039; doesn&#039;t exist in engine
+-- Error reading data for table cineticket.seats: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `cineticket`.`seats`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+-- Error reading structure for table cineticket.users: #1932 - Table &#039;cineticket.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table cineticket.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `cineticket`.`users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `__efmigrationshistory`
+--
+-- Error reading structure for table cineticket.__efmigrationshistory: #1932 - Table &#039;cineticket.__efmigrationshistory&#039; doesn&#039;t exist in engine
+-- Error reading data for table cineticket.__efmigrationshistory: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `cineticket`.`__efmigrationshistory`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
